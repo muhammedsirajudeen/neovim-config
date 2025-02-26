f634aed93389b0cf339e308d004f586f71c96240
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-02-26 13:09:26 +0530

    feat: initial setup of my neovim config

---
+++ init.lua
@@ -0,0 +1,112 @@
+-- Set leader key
+vim.g.mapleader = " "
+
+-- Lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+-- Load plugins
+require("lazy").setup("plugins")
+require("lspconfig").tailwindcss.setup{}
+
+-- Basic settings
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.termguicolors = true
+vim.opt.background = "dark"
+vim.cmd("highlight Normal guibg=black")
+vim.cmd("highlight NonText guibg=black")
+vim.cmd("highlight NvimTreeNormal guibg=black")
+vim.cmd("highlight NvimTreeEndOfBuffer guibg=black")
+
+--my function definition
+local function goto_source()
+  require("telescope.builtin").find_files()
+end
+vim.keymap.set("n", "fs", goto_source, { noremap = true, silent = true })
+
+-- Insert date time
+local function date_time_insert()
+  local date = os.date("%Y-%m-%d")
+  vim.api.nvim_put({date},"",true,true)
+end
+
+vim.keymap.set("!","~!",date_time_insert,{noremap=true,silent=true})
+
+
+vim.keymap.set("n", "gs", function()
+  require("telescope.builtin").lsp_definitions()
+end, { noremap = true, silent = true })
+
+-- These are all custom keybindings 
+vim.api.nvim_set_keymap('n', '<leader>cs', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+
+local popup_win = nil
+local popup_buf = nil
+
+function ClosePopup()
+    if popup_win then
+        vim.api.nvim_win_close(popup_win, true)
+        popup_win = nil
+        popup_buf = nil
+    end
+end
+ 
+function ShowPopup()
+    if popup_win then return end
+
+    popup_buf = vim.api.nvim_create_buf(false, true)
+    local width = 50
+    local height = 10
+    local opts = {
+        relative = 'editor',
+        width = width,
+        height = height,
+        col = (vim.o.columns - width) / 2,
+        row = (vim.o.lines - height) / 2,
+        style = 'minimal',
+        border = 'rounded',
+    }
+
+    popup_win = vim.api.nvim_open_win(popup_buf, true, opts)
+
+    -- Define your keybindings for display
+    local keybindings = {
+        "Keybinding Reference:",
+        "----------------------",
+        "<leader>cs -> Search Text (Telescope live_grep)",
+        "<leader>fb -> Open Buffers (Telescope)",
+        "<leader>fh -> Help Tags (Telescope)",
+        "fs -> Find Files (Telescope)",
+        "gs -> Go to LSP Definition",
+        "~! -> Insert Current Date",
+        "<leader>h -> Show This Popup",
+        "",
+        "[ Press ENTER to Close ]"
+    }
+
+    vim.api.nvim_buf_set_lines(popup_buf, 0, -1, false, keybindings)
+
+    -- Set background color to black
+    vim.api.nvim_win_set_option(popup_win, "winhl", "Normal:PopupBackground")
+
+    -- Define the highlight group
+    vim.cmd("highlight PopupBackground guibg=#000000 guifg=#FFFFFF")
+
+    -- Map <Enter> key inside the floating window to close it
+    vim.api.nvim_buf_set_keymap(popup_buf, 'n', '<CR>', ':lua ClosePopup()<CR>', { noremap = true, silent = true })
+end
+
+vim.api.nvim_set_keymap('n', '<leader>h', ':lua ShowPopup()<CR>', { noremap = true, silent = true })
+
---
+++ lua/plugins/init.lua
@@ -0,0 +1,129 @@
+return {
+  -- Theme
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, config = function()
+    vim.cmd("colorscheme tokyonight")
+  end },
+
+  -- File Icons
+  { "nvim-tree/nvim-web-devicons" },
+
+  -- File Explorer
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" }, config = function()
+    require("nvim-tree").setup()
+  end },
+
+  -- Status Line
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, config = function()
+    require("lualine").setup()
+  end },
+
+  -- Git Integration
+  { "lewis6991/gitsigns.nvim", config = function()
+    require("gitsigns").setup({
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      current_line_blame = true, -- Show inline blame
+    })
+  end },
+
+  { "tpope/vim-fugitive" }, -- Git commands like :Git, :Gdiff, :Gblame, etc.
+
+  -- LSP & Autocomplete
+  { "neovim/nvim-lspconfig", config = function()
+    local lspconfig = require("lspconfig")
+
+    -- Set diagnostic icons
+    local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+
+    -- Enable TypeScript & React LSP (use ts_ls instead of tsserver)
+    lspconfig.ts_ls.setup({
+      filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+      root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+    })
+
+    -- Enable ESLint
+    lspconfig.eslint.setup({
+      on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          command = "EslintFixAll",
+        })
+      end,
+    })
+  end },
+
+  -- Autocomplete
+  { "hrsh7th/nvim-cmp", dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "onsails/lspkind.nvim" -- Icons for autocomplete
+    }, config = function()
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+
+      cmp.setup({
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text", -- Show text + icon
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+  end },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        user_default_options = { names = false },
+      })
+    end,
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end,
+  }
+}
+
